0f14e7bf2fa2e08a4cfdac397d744f1679a1a50f
Paul Smith <code@uvwxy.de>
2019-12-03 22:26:25 +0100

    step 2: include inkplate6 lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <Arduino.h>
+#include "Inkplate.h"
+#include "driver/rtc_io.h"
+
+Inkplate display(INKPLATE_1BIT);
 
 void setup() {
+  Wire.begin();
   Serial.begin(115200);
 
-  // TODO: setup MCP23017
-
-  // TODO: setup e-paper panel
+  display.draw_mode_on();
+  display.begin();
+  display.clean();
+  for (int i = 0; i < 8; i++) {
+    display.setTextColor(i, 8 - i);
+    display.setTextSize(i);
+    display.setCursor(0, (600 / 8) * i);
+    display.print("Hello World");
+  }
+  display.display();
+  display.draw_mode_off();
+  esp_sleep_enable_timer_wakeup(60 * 1000 * 1000);
+  rtc_gpio_isolate(GPIO_NUM_12);
+  esp_deep_sleep_start();
 }
 
-void loop() {
-  Serial.println("Hello World");
-  delay(1000);
-  // TODO: read buttons
-
-  // TODO: draw on e-paper panel
-}
+void loop() {}
